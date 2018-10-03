e85cbf9566a3d5bbee0921294e42e0cb9f3b9709
Ryan Donohue <ryan@rdonohue.ca>
2018-10-02 20:19:04 -0600

    feat(AdType): Add, update, delete vendor types

--- src/components/add-edit-ad-type/AddEditAdType.vue
+++ src/components/add-edit-ad-type/AddEditAdType.vue
@@ -5,11 +5,11 @@
       <div class="col-12">
         <form>
           <div class="form-group">
-            <label>Type</label>
+            <label>Type  <span class="text-danger" v-if="errors.type">{{errors.type}}</span></label>
             <input type="text" v-model="adType.type" class="form-control" />
           </div>
           <div class="form-group">
-            <label>Type</label>
+            <label>description  <span class="text-danger" v-if="errors.type">{{errors.type}}</span></label>
             <textarea type="text" v-model="adType.description" class="form-control"></textarea>
           </div>
           <div class="row">
@@ -34,43 +34,52 @@ export default {
   data() {
     return {
       adTypeId: this.$props.id,
-      adType: {}
+      adType: {},
+      errors: {}
     };
   },
   created: function() {
     if (this.adTypeId) {
-      this.$http
-        .get(`/api/adTypes/${this.adTypeId}`)
-        .then(({ body }) => {
-          this.adType = body;
-        });
+      this.$http.get(`/api/adTypes/${this.adTypeId}`).then(({ body }) => {
+        this.adType = body;
+      });
     } else {
       this.adTypeId = null;
     }
   },
   methods: {
     save() {
-      this.$http
-        .post("/api/adTypes", this.adType)
-        .then(({ status }) => {
+      this.$http.post("/api/adTypes", this.adType).then(
+        ({ status }) => {
           if (status === 200) {
             this.$router.push({ name: "ad-management" });
           }
-        });
+        },
+        response => {
+          this.errors = response.body;
+          this.$toasted.show(
+            "There were some errors :( please review and try again!"
+          );
+        }
+      );
     },
     update() {
       this.$http
         .put(`/api/adTypes/${this.adTypeId}`, this.adType)
         .then(({ body }) => {
           this.$router.push({ name: "ad-management" });
-        });
+        }),
+        response => {
+          this.errors = response.body;
+          this.$toasted.show(
+            "There were some errors :( please review and try again!"
+          );
+        };
     },
     remove() {
-      this.$http
-        .delete(`/api/adTypes/${this.adTypeId}`)
-        .then(({ body }) => {
-          this.$router.push({ name: "ad-management" });
-        });
+      this.$http.delete(`/api/adTypes/${this.adTypeId}`).then(({ body }) => {
+        this.$router.push({ name: "ad-management" });
+      });
     }
   }
 };
